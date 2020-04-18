dc1499910348089ef3a5618c9fc715112fcca4e9
taewonee <twoyearsone@gmail.com>
2020-04-19 03:35:57 +0900

    for web page

---
+++ build/precache-manifest.0de34a6fb7dfbeed07270bb3a2afcba0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfa14eecc655db6b2770d669d3f41667",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79616e8a2acc695d9ab6",
+    "url": "/static/css/main.bcd2e130.chunk.css"
+  },
+  {
+    "revision": "708010d58d799d68afc3",
+    "url": "/static/js/2.f128b2f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f128b2f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79616e8a2acc695d9ab6",
+    "url": "/static/js/main.c38ad0c9.chunk.js"
+  },
+  {
+    "revision": "0f7e17235ce2106ccee1",
+    "url": "/static/js/runtime-main.549be430.js"
+  },
+  {
+    "revision": "4d6d7f780e1ad2dfeb352d9bb3aa621a",
+    "url": "/static/media/ping1.4d6d7f78.jpg"
+  },
+  {
+    "revision": "729d9e3953650fadc5960e72e65121a8",
+    "url": "/static/media/ping2.729d9e39.jpg"
+  }
+]);
